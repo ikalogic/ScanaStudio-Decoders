02101fc971c8de9fc6894e2d43039e7b92ccb420
Kosinov Vladislav <vladislav@ikalogic.com>
2016-02-03 17:08:44 +0100

    Bug fix and new features for NMEA

--- nmea0183.js
+++ nmea0183.js
@@ -15,6 +15,7 @@ The following commented block allows some related informations to be displayed o
 
 <RELEASE_NOTES>
 
+	V1.2: Fixed some minor bugs. Added support of GPGGA, GPGSV and GPRMC frames.
 	V1.1: Added Packet/Hex View support.
 	V1.0: Initial release.
 
@@ -26,6 +27,12 @@ The following commented block allows some related informations to be displayed o
 
 </AUTHOR_URL>
 						
+*************************************************************************************
+*/
+
+/*
+*************************************************************************************
+								      INFO
 *************************************************************************************
 */
 
@@ -41,7 +48,7 @@ function get_dec_name()
  */
 function get_dec_ver()
 {
-	return "1.1";
+	return "1.2";
 }
 
 
@@ -52,9 +59,27 @@ function get_dec_auth()
 	return "IKALOGIC";
 }
 
+/*
+*************************************************************************************
+							    GLOBAL VARIABLES
+*************************************************************************************
+*/
 
- /* graphical user interface
- */
+var PKT_COLOR_DATA;
+var PKT_COLOR_GENER_TITLE;
+var PKT_COLOR_GPGGA_TITLE;
+var PKT_COLOR_GPGSA_TITLE;
+var PKT_COLOR_GPGSV_TITLE;
+var PKT_COLOR_GPRMC_TITLE;
+
+/*
+*************************************************************************************
+								   DECODER
+*************************************************************************************
+*/
+
+/* graphical user interface
+*/
 function gui()
 {
 	ui_clear();	// clean up the User interface before drawing a new one.
@@ -65,29 +90,8 @@ function gui()
 	ui_add_txt_combo("invert","Inverted logic");
 		ui_add_item_to_txt_combo("Non inverted logic",true);
 		ui_add_item_to_txt_combo("Inverted logic");
+
 }
-
-
-var NMEA_OUT_MSG =
-{
-	GGA : {strId: "GPGGA"},
-	GSA : {strId: "GPGSA"},
-	GSV : {strId: "GPGSV"},
-	RMC : {strId: "GPRMC"}
-};
-
-
-/* Constants 
-*/
-var NMEA_ST_OF_MSG  = 0x24;		// '$' character code
-var NMEA_END_OF_MSG = 0x0A;		// 'LF' character code
-
-var PKT_COLOR_DATA;
-var PKT_COLOR_GENER_TITLE;
-var PKT_COLOR_GPGGA_TITLE;
-var PKT_COLOR_GPGSA_TITLE;
-var PKT_COLOR_GPGSV_TITLE;
-var PKT_COLOR_GPRMC_TITLE;
 
 
 /* This is the function that will be called from ScanaStudio
@@ -108,11 +112,11 @@ function decode()
 	PKT_COLOR_GENER_TITLE = dark_colors.gray;
 	PKT_COLOR_GPGGA_TITLE = dark_colors.blue;
 	PKT_COLOR_GPGSA_TITLE = dark_colors.orange;
-	KT_COLOR_GPGSV_TITLE  = dark_colors.green;
+	PKT_COLOR_GPGSV_TITLE = dark_colors.green;
 	PKT_COLOR_GPRMC_TITLE = dark_colors.violet;
 
 	get_ui_vals();
-
+	
 	var decBuf = pre_decode("uart.js", "ch = " + ch + ";"
                                     + "baud = " + baud + ";"
 									+ "nbits = " + 3 + ";"		// 8-bit word
@@ -128,12 +132,12 @@ function decode()
 
 		if (uartData.data.length > 0)
 		{
-			if (uartData.data == NMEA_ST_OF_MSG)
+			if (String.fromCharCode(uartData.data) == "$")
 			{
 				var msg = String.fromCharCode(uartData.data);
 				var stOfMsg = uartData.start_s, endOfMsg;
 
-				while (uartData.data != NMEA_END_OF_MSG)
+				while ((String.fromCharCode(uartData.data) != "\r") && (decBuf.length > uartDataCnt))
 				{
 					uartData = decBuf[uartDataCnt];
 					uartDataCnt++;
@@ -154,9 +158,7 @@ function decode()
 				dec_item_new(ch, stOfMsg , endOfMsg);
 				dec_item_add_pre_text(msg);
 				dec_item_add_pre_text(msgId);
-				dec_item_add_comment(msg);
 
-				/*
 				pkt_start("NMEA 0183");
 
 				if (msgId == "GPGGA")
@@ -165,33 +167,297 @@ function decode()
 					pkt_start(msgId);
 
 					var utc = outMsgArr.shift();
-					utc = utc.slice(0, 2) + ":" +  utc.slice(2, 4) + ":" + utc.slice(4, 6) + "." + utc.slice(7, 10);
 
-					pkt_add_item(-1, -1, "UTC TIME", utc, PKT_COLOR_GPGGA_TITLE, PKT_COLOR_DATA);
+					if (utc.length > 0)
+					{
+						utc = utc.slice(0, 2) + ":" +  utc.slice(2, 4) + ":" + utc.slice(4, 6) + "." + utc.slice(7, 10);
+					}
+
+					add_pkt_data(utc, "", "UTC TIME", PKT_COLOR_GPGGA_TITLE);
 
 					var latitude = outMsgArr.shift();
-					var dir = outMsgArr.shift();
-					dir = "N";
-					latitude = "4124.8963";
-					latitude = latitude.slice(0, 2) + "°" + latitude.slice(2, 4) + "'" + latitude.slice(5, 9) + '"';
+					var ns = outMsgArr.shift();
 
-					pkt_add_item(-1, -1, "LATITUDE", latitude + dir, PKT_COLOR_GPGGA_TITLE, PKT_COLOR_DATA);
+					if (latitude.length > 0)
+					{
+						latitude = latitude.slice(0, 2) + "°" + latitude.slice(2, 4) + "'" + latitude.slice(5, 9) + '"';
+					}
+
+					add_pkt_data(latitude, ns, "LATITUDE", PKT_COLOR_GPGGA_TITLE);
 
 					var longitude = outMsgArr.shift();
-					dir = outMsgArr.shift();
-					dir = "W";
-					longitude = "8151.6838";
-					longitude = longitude.slice(0, 2) + "°" + longitude.slice(2, 4) + "'" + longitude.slice(5, 9) + '"';
+					var ew = outMsgArr.shift();
 
-					pkt_add_item(-1, -1, "LONGITUDE", longitude + dir, PKT_COLOR_GPGGA_TITLE, PKT_COLOR_DATA);
+					if (longitude.length > 0)
+					{
+						longitude = longitude.slice(0, 2) + "°" + longitude.slice(2, 4) + "'" + longitude.slice(5, 9) + '"';
+					}
+
+					add_pkt_data(longitude, ew, "LONGITUDE", PKT_COLOR_GPGGA_TITLE);
+
+					var posFix = outMsgArr.shift();
+					var posFixStr;
+
+					switch (posFix)
+					{
+						case "0": posFixStr = "Fix not available or invalid"; break;
+						case "1": posFixStr = "SPS Mode"; break;
+						case "2": posFixStr = "Diff SPS Mode"; break;
+						case "3": posFixStr = "PPS Mode"; break;
+					}
+
+					add_pkt_data(posFixStr, "", "POS FIX IND", PKT_COLOR_GPGGA_TITLE);
+
+					var satNum = outMsgArr.shift();
+					add_pkt_data(satNum, "", "TOTAL SAT", PKT_COLOR_GPGGA_TITLE);
+
+					var hdop = outMsgArr.shift();
+					add_pkt_data(satNum, "", "HDOP", PKT_COLOR_GPGGA_TITLE);
+
+					var altitude = outMsgArr.shift();
+					var units = outMsgArr.shift();
+					add_pkt_data(altitude, (" " + units), "ALTITUDE", PKT_COLOR_GPGGA_TITLE);
+
+					var geoid = outMsgArr.shift();
+					units = outMsgArr.shift();
+					add_pkt_data(geoid, (" " + units), "GEOID SEP", PKT_COLOR_GPGGA_TITLE);
+
+					var ageDiff = outMsgArr.shift();
+					add_pkt_data(ageDiff, "", "AGE DIFF", PKT_COLOR_GPGGA_TITLE);
+
+					var checksum = verify_checksum(msg);
+					pkt_add_item(-1, -1, "CHECKSUM", checksum, PKT_COLOR_GPGGA_TITLE, PKT_COLOR_DATA);
+
+					pkt_end();
+				}
+
+				if (msgId == "GPGSV")
+				{
+					pkt_add_item(-1, -1, msgId, "", PKT_COLOR_GPGSV_TITLE, PKT_COLOR_DATA);
+					pkt_start(msgId);
+
+					var numMsg = outMsgArr.shift();					
+					add_pkt_data(numMsg, "", "TOTAL MSG", PKT_COLOR_GPGSV_TITLE);
+
+					var msgNum = outMsgArr.shift();
+					add_pkt_data(msgNum, "", "MSG NUM", PKT_COLOR_GPGSV_TITLE);
+
+					var satNum = outMsgArr.shift();
+					add_pkt_data(satNum, "", "TOTAL SAT", PKT_COLOR_GPGSV_TITLE);
+
+					do
+					{
+						var satId = outMsgArr.shift();
+						add_pkt_data(satId, "", "SAT ID", PKT_COLOR_GPGSV_TITLE);
+						if (outMsgArr[0] == "*") break;
+
+						var elevation = outMsgArr.shift();						
+						add_pkt_data(elevation, " deg", "ELEVATION", PKT_COLOR_GPGSV_TITLE);
+						if (outMsgArr[0] == "*") break;
+
+						var azimuth = outMsgArr.shift();
+						add_pkt_data(azimuth, " deg", "AZIMUTH", PKT_COLOR_GPGSV_TITLE);
+						if (outMsgArr[0] == "*") break;
+
+						var snr = outMsgArr.shift();
+						add_pkt_data(azimuth, " dB", "SNR", PKT_COLOR_GPGSV_TITLE);
+						if (outMsgArr[0] == "*") break;
+
+					} while (outMsgArr.length > 0);
+
+					var checksum = verify_checksum(msg);
+					pkt_add_item(-1, -1, "CHECKSUM", checksum, PKT_COLOR_GPGSV_TITLE, PKT_COLOR_DATA);
+
+					pkt_end();
+				}
+
+				if (msgId == "GPRMC")
+				{
+					pkt_add_item(-1, -1, msgId, "", PKT_COLOR_GPRMC_TITLE, PKT_COLOR_DATA);
+					pkt_start(msgId);
+
+					if (isEmpty(outMsgArr)) break;
+					var utc = outMsgArr.shift();
+
+					if (utc.length > 0)
+					{
+						utc = utc.slice(0, 2) + ":" +  utc.slice(2, 4) + ":" + utc.slice(4, 6) + "." + utc.slice(7, 10);
+					}
+
+					add_pkt_data(utc, "", "UTC TIME", PKT_COLOR_GPRMC_TITLE);
+
+					if (isEmpty(outMsgArr)) break;
+					var stat = outMsgArr.shift();
+
+					if (stat.length > 0)
+					{
+						if (stat == "A") 
+						{
+							stat = "data valid";
+						}
+						else if (stat == "V")
+						{
+							stat = "data not valid";
+						}
+						else
+						{
+							stat = "invalid arg";
+						}	
+					}
+
+					add_pkt_data(stat, "", "STATUS", PKT_COLOR_GPRMC_TITLE);
+
+					if (isEmpty(outMsgArr)) break;
+					var latitude = outMsgArr.shift();
+					if (isEmpty(outMsgArr)) break;
+					var ns = outMsgArr.shift();
+
+					if (latitude.length > 0)
+					{
+						latitude = latitude.slice(0, 2) + "°" + latitude.slice(2, 4) + "'" + latitude.slice(5, 9) + '"';
+					}
+
+					add_pkt_data(latitude, ns, "LATITUDE", PKT_COLOR_GPRMC_TITLE);
+
+					if (isEmpty(outMsgArr)) break;
+					var longitude = outMsgArr.shift();
+					if (isEmpty(outMsgArr)) break;
+					var ew = outMsgArr.shift();
+
+					if (longitude.length > 0)
+					{
+						longitude = longitude.slice(0, 2) + "°" + longitude.slice(2, 4) + "'" + longitude.slice(5, 9) + '"';
+					}
+
+					add_pkt_data(longitude, ew, "LONGITUDE", PKT_COLOR_GPRMC_TITLE);
+
+					if (isEmpty(outMsgArr)) break;
+					var speed = outMsgArr.shift();
+					add_pkt_data(speed, " knots", "SPEED", PKT_COLOR_GPRMC_TITLE);
+
+					if (isEmpty(outMsgArr)) break;
+					var course = outMsgArr.shift();
+					add_pkt_data(course, " deg", "COURSE", PKT_COLOR_GPRMC_TITLE);
+
+					if (isEmpty(outMsgArr)) break;
+					var date = outMsgArr.shift();
+
+					if (date.length > 0)
+					{
+						date = date.slice(0, 2) + "/" +  date.slice(2, 4) + "/" + date.slice(4, 6);
+					}
+
+					add_pkt_data(date, "", "DATE", PKT_COLOR_GPRMC_TITLE);
+
+					if (isEmpty(outMsgArr)) break;
+					var magVar = outMsgArr.shift();
+					add_pkt_data(magVar, " deg", "MAGN VAR", PKT_COLOR_GPRMC_TITLE);
+
+					if (isEmpty(outMsgArr)) break;
+					var ewInd = outMsgArr.shift();
+					add_pkt_data(ewInd, "", "E/W IND", PKT_COLOR_GPRMC_TITLE);
+
+					if (isEmpty(outMsgArr)) break;
+					var checksum = verify_checksum(msg);
+					pkt_add_item(-1, -1, "CHECKSUM", checksum, PKT_COLOR_GPRMC_TITLE, PKT_COLOR_DATA);
 
 					pkt_end();
 				}
 
 				pkt_end();
-				*/
 			}
 		}
+	}
+}
+
+
+/*
+*/
+function add_pkt_data (data, suffix, param, color)
+{
+	if (data !== undefined)
+	{
+		if (data.length > 0)
+		{
+			if (suffix.length > 0)
+			{
+				data = data + suffix;
+			}
+		}
+		else
+		{
+			data = "none";
+		}
+
+		pkt_add_item(-1, -1, param, data, color, PKT_COLOR_DATA);
+	}
+}
+
+
+/*
+*/
+function verify_checksum (data)
+{	
+	var str;
+
+	if (data.indexOf('*') === -1)
+	{
+		str = "none";
+	}
+	else
+	{
+		var checksum1 = 0;
+		var dataStr =  data.substring(data.lastIndexOf("$") + 1, data.lastIndexOf("*"));
+
+		str = data.substring(data.lastIndexOf("*") + 1, data.lastIndexOf("\r"));
+		var checksum2 = parseInt(str, 16);
+
+		for (var i = 0; i < dataStr.length; i++) 
+		{
+			checksum1 = checksum1 ^ dataStr.charCodeAt(i);
+		}
+
+		if (str.length > 0)
+		{
+			str = "0x" + str;
+
+			if (checksum1 == checksum2)
+			{
+				str = str + " (OK)";
+			}
+			else
+			{
+				str = str + " (WRONG)";
+			}
+		}
+		else
+		{
+			str = "NONE";
+		}
+	}
+
+	return str;
+}
+
+
+/*
+*************************************************************************************
+							        UTILS
+*************************************************************************************
+*/
+
+/*
+*/
+function isEmpty (arr)
+{
+	if (arr.length > 0)
+	{
+		return false;
+	}
+	else
+	{
+		pkt_end();
+		return true;
 	}
 }
 
